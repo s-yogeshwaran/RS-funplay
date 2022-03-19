943623c850620297b8d03255b53d302797aa5e0c
s-yogeshwaran <95536746+s-yogeshwaran@users.noreply.github.com>
2022-03-19 10:29:47 +0530

    Update rotateScreencode.py

--- rotateScreencode.py
+++ rotateScreencode.py
@@ -1,10 +1,14 @@
 import rotatescreen
 import time
+import streamlit as st
+
+st.title("Welcome to funplay :)")
 
 screen = rotatescreen.get_primary_display()
 start_pos = screen.current_orientation
 
-for i in range(1,5):
-    pos = abs((start_pos - i*90) % 360)
-    screen.rotate_to(pos)
-    time.sleep(0.5)
+if st.button("click me!, and have fun"):
+    for i in range(1,5):
+        pos = abs((start_pos - i*90) % 360)
+        screen.rotate_to(pos)
+        time.sleep(0.5)
